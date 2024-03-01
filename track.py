62ecece1221a12d46e6b70cb1b17ac1aef5ef637
Jack <jackthor0608@gmail.com>
2024-02-29 22:49:22 -0500

    Update track.py

--- track.py
+++ track.py
@@ -22,6 +22,7 @@ def main():
             online_availability = product_info.get('onlineAvailability')
             if online_availability:
                 print("Available at " + str(datetime.datetime.now()))
+                # Comment out the following line if not using SMS feature
                 SMS.send("Best Buy: Product " + product_sku + " available at " + str(datetime.datetime.now()))
         except Exception as e:
             print("An error occurred with the request:", e)
